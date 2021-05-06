04de86da67d63d58a794640ef4876baf93cdf5bb
Gaël <lfg.lefortgael@gmail.com>
2021-05-06 16:16:48 +0200

    V2

---
+++ auth/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded());
+
+app.post("/auth", function (req, res) {
+  /* This server is only available to nginx */
+  const streamkey = req.body.key;
+
+  /* You can make a database of users instead :) */
+  if (streamkey === "supersecret") {
+    res.status(200).send();
+    return;
+  }
+
+  /* Reject the stream */
+  res.status(403).send();
+});
+
+app.listen(8000, function () {
+  console.log("Listening on port 8000!");
+});
